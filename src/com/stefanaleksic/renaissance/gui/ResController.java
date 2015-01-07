c969af59814506a5f4f75b9f0dae142028b16752
Stefan <soccerfanatic1996@gmail.com>
2015-01-06 22:32:30 -0500

    adding MVC pattern for GUI

---
+++ src/com/stefanaleksic/renaissance/gui/ResController.java
@@ -0,0 +1,8 @@
+package com.stefanaleksic.renaissance.gui;
+
+/**
+ * Created by Stefan on 1/6/2015.
+ *
+ */
+public class ResController {
+}
---
+++ src/com/stefanaleksic/renaissance/gui/ResView.java
@@ -0,0 +1,16 @@
+package com.stefanaleksic.renaissance.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Stefan on 1/6/2015.
+ *
+ */
+public class ResView extends JFrame{
+    /**
+     * Sets up the view.
+     */
+    public ResView(){
+
+    }
+}
